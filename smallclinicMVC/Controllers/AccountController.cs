53c678f44ea1dbfae7162964b4e79013e047e157
aqib <developers3nd@gmail.com>
2023-08-12 06:11:59 +0500

    Clinic System ASP NET MVC

---
+++ smallclinicMVC/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using smallclinicMVC.Models;
+
+namespace smallclinicMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        dbClinicSystemEntities db = new dbClinicSystemEntities();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Email, string Password)
+        {
+            var query = db.Users.Where(m => m.Email == Email && m.Password == Password).SingleOrDefault();
+
+            if (query != null)
+            {
+                Session["UserID"] = query.UserID;
+                Session["Email"] = query.Email;
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                TempData["msg"] = "Invalid Email & password";
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Account");
+        }
+	}
+}
---
+++ smallclinicMVC/Controllers/TokenController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using smallclinicMVC.Models;
+
+namespace smallclinicMVC.Controllers
+{
+    public class TokenController : Controller
+    {
+        dbClinicSystemEntities db = new dbClinicSystemEntities();
+        public ActionResult Index()
+        {
+            var query = db.token_details.ToList();
+
+            return View(query);
+        }
+
+        public ActionResult Create()
+        {
+            List<Doctor> doctorList = db.Doctors.ToList();
+            ViewBag.doctorlst = new SelectList(doctorList, "DoctorID", "LastName");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Token t)
+        {
+            List<Doctor> doctorList = db.Doctors.ToList();
+            ViewBag.doctorlst = new SelectList(doctorList, "DoctorID", "LastName");
+
+            if (t != null)
+            {
+                Random r = new Random();
+                int num = r.Next();
+                t.UniqueToken = num.ToString();
+
+                TempData["tokenNumber"] = num.ToString();
+
+                db.Tokens.Add(t);
+                db.SaveChanges();
+            }
+            else
+            {
+                TempData["tokenNumber"] = "token failed";
+            }
+         
+            return View();
+        }
+
+        public JsonResult GetReport(DateTime str, DateTime end)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+
+            // select * from token where date between  '' and ''
+            var query = from x in db.token_details where x.TokenDate >= str && x.TokenDate <= end select x;
+
+            return Json(query,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Reporting()
+        {
+            return View();
+        }
+	}
+}
